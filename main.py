7b2d490c298ad5e18c0b68e0987a27d42cdea5ff
Shrey <ptl.shreys@gmail.com>
2025-08-12 01:49:22 +0200

    Here I just did a warm-up exercise with my turtle (ahem-ahem). And we drew a simple square for the fun of it.

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 from turtle import *
+
+timmy_turtle = Turtle()
+timmy_turtle.shape("turtle")
+timmy_turtle.color("lime green")
+
+def draw_a_square():
+    timmy_turtle.fd(100)
+    timmy_turtle.right(90)
+    timmy_turtle.fd(100)
+    timmy_turtle.right(90)
+    timmy_turtle.fd(100)
+    timmy_turtle.right(90)
+    timmy_turtle.fd(100)
+    timmy_turtle.right(90)
+
+draw_a_square()
+screen = Screen()
+screen.exitonclick()
